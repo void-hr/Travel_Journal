a367067a3473a28b652ac4e508d6454ac7d3446e
void-hr <gsharmaji93@gmail.com>
2022-09-01 22:23:54 +0530

    navbar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function Navbar(){
+    return (
+        <div className="nav" style={{backgroundColor:'#F55A5A', height:'55px', display:'flex', justifyContent:'center',alignItems:'center', color:'#f5f5f5f5'}}>
+            <i class="fa-solid fa-earth-americas" style={{fontSize:'24px'}}></i>
+            <p style={{fontSize:'14.46px', paddingLeft:'7px', fontWeight:'400'}}>my travel journal.</p>
+        </div>
+
+
+    )
+}
